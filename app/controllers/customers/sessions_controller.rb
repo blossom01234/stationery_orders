aa831515f3c5fe5d30afd3ef431b6c67c24b9c3c
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2024-06-27 19:58:40 +0900

    ログアウトをカスタマーとユーザーで分岐

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -25,7 +25,6 @@ class Customers::SessionsController < Devise::SessionsController
 
   private
     def after_sign_out_path_for(resource_or_scope)
-      debugger
       new_customer_session_path
     end
 
