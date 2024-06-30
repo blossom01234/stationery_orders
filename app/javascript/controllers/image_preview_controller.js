f91680ed11495e182f32f552909effffe53770d7
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2024-06-30 13:57:17 +0900

    画像プレビュー、画像クリア処理追加

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -1,19 +1,36 @@
-import { Controller } from "@hotwired/sitmulus"
+import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="image-preview"
 export default class extends Controller {
-    static target  = ["input", "preview"]
-
+    static targets = ["input", "preview", "registeredImage"]
+  
     connect() {
-        this.inputTarget.addEventListener('chage', this.showPreview.bind(this))
+        this.inputTarget.addEventListener('change', this.showPreview.bind(this))
     }
-
+  
     showPreview(event) {
-        const file = event.target.files[0]
-
-        if (file) {
-            const reader = new FileReader()
-
-            reader.onload = ()
+      const file = event.target.files[0]
+  
+      if (file) {
+        const reader = new FileReader()
+  
+        reader.onload = (e) => {
+          this.previewTarget.src = e.target.result
+          this.previewTarget.style.display = 'block'
+          this.registeredImageTarget.src = ''
+          this.registeredImageTarget.style.display = 'none'
         }
+  
+        reader.readAsDataURL(file)
+      } else {
+        this.previewTarget.src = ''
+        this.previewTarget.style.display = 'none'
+      }
     }
-}
+
+    clearPreview() {
+        this.inputTarget.value= ''
+        this.previewTarget.src = ''
+        this.previewTarget.style.display = 'none'
+    }
+  }
