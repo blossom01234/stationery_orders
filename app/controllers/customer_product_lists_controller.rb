3acfe0bc5ace79a0fe1311e3939425890476bc71
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2024-07-06 17:55:32 +0900

    画像取得のn+1問題修正

--- app/controllers/customer_product_lists_controller.rb
+++ app/controllers/customer_product_lists_controller.rb
@@ -2,6 +2,7 @@ class CustomerProductListsController < ApplicationController
     before_action :check_customer_sign_in_status
     def index
         @products = Product.includes(:maker)
+            .with_attached_image
             .references(:makers)
             .paginate(page: params[:page], per_page: 20)
     end
