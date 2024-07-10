1024afe8196896905e19156e96e4bed64946e4b0
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2024-07-10 22:45:01 +0900

    カート内小計と合計計算

--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -1,13 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["quantity", "subtotal"]
+  static targets = ["quantity", "subtotal", "price"]
+  connect() {
+    this.updateTotal();
+  }
 
   update() {
-    const quantity = parseFloat(this.quantityTarget.value) || 0;
-    const price = parseFloat(this.priceTarget.dataset.price) || 0;
-    const subtotal = (quantity * price).toFixed(2);
+    const quantity = parseInt(this.quantityTarget.value) || 0;
+    const price = parseInt(this.priceTarget.textContent) || 0;
+    const subtotal = (quantity * price);
 
     this.subtotalTarget.textContent = subtotal;
+
+    this.updateTotal();
+  }
+
+  updateTotal() {
+    let total = 0;
+    this.subtotalTargets.forEach(subtotal => {
+      total += parseInt(subtotal.innerText);
+    })
+    document.querySelector(".cart-total").innerText = total;
   }
 }
