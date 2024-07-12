552710b8a3086cb4dce6cfd8a09643b46da5c0d9
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2024-07-12 23:24:13 +0900

    受注編集画面追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:edit, :update, :destroy]
+
   def index
     @orders = Order.paginate(page: params[:page], per_page: 15)
+  end
+
+  def edit
   end
 
   def show
@@ -11,4 +16,9 @@ class OrdersController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_order
+      @order = Order.find(params[:id])
+    end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+
+  def customer_full_name
+    "#{customer.try(:first_name)} #{customer.try(:last_name)}"
+  end
 end
