b4848e7f24ae28f6c57682e534bfa910c7f218da
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2024-07-15 22:53:58 +0900

    会員更新項目

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,9 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     has_many :orders, dependent: :destroy
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
